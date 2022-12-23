88c271e379076144a7317309304c6e9207130008
Fasero11 <gon2d3@gmail.com>
2022-12-23 19:02:23 +0100

    combined proxys

---
+++ practica4/proxy_client.c
@@ -0,0 +1,253 @@
+// Gonzalo Vega Pérez - 2022
+
+#include "proxy_client.h"
+struct sockaddr_in addr;
+char proc_name[32];
+int client_socket;
+int mode;
+char client_topic[100];
+int client_id;
+int client_type;
+
+void sighandler(int signum){
+    DEBUG_PRINTF("SIGNAL RECEIVED...  bye.\n");
+    struct timespec time;
+    long seconds, nanoseconds;
+
+    struct message message;
+    struct response response;
+
+    if (client_type == PUBLISHER){
+        message.action = UNREGISTER_PUBLISHER;
+    } else {
+        message.action = UNREGISTER_SUBSCRIBER;
+    }
+
+    strcpy(message.topic, client_topic);
+    message.id = client_id;
+
+    // Send Unregister until an OK response is received.
+    do {
+        if (send(client_socket, (void *)&message, sizeof(message), 0) < 0){
+            warnx("send() failed. %s\n", strerror(errno));
+            exit(1);
+        }
+
+        if (recv(client_socket, (void *)&response, sizeof(response), 0) < 0){
+            warnx("recv() failed. %s\n", strerror(errno));
+            exit(1);
+        }
+    } while (response.response_status != OK);
+
+    clock_gettime(CLOCK_REALTIME, &time);
+    seconds = time.tv_sec;
+    nanoseconds = time.tv_nsec;
+
+    printf("[%ld.%ld] De-Registrado (%d) correctamente del broker.\n",
+    seconds, nanoseconds, client_id); 
+
+    close(client_socket);
+    exit(1);
+}
+
+void transfer_publisher(){
+    DEBUG_PRINTF("Publishing...\n");  
+    struct publish publish;
+    struct message message;
+    struct timespec time, time_epoch;
+    long time_seconds, time_seconds_epoch,time_nanoseconds, time_nanoseconds_epoch;
+
+    message.action = PUBLISH_DATA;
+
+    FILE* ptr;
+    ptr = fopen("/proc/loadavg", "r");
+
+    while(1){
+
+        sleep(3);
+
+        clock_gettime(CLOCK_REALTIME, &time);        // Get monotic time (used in rest of the program)
+        clock_gettime(CLOCK_REALTIME, &time_epoch);   // Get epoch time (For msg generated time)
+        
+        // Generate Data
+        if (ptr != NULL){
+            fread(publish.data, sizeof(char), 100, ptr);
+        }
+
+        DEBUG_PRINTF("DATA READ: %s\n",publish.data);
+
+        time_seconds_epoch = time_epoch.tv_sec;
+        time_nanoseconds_epoch = time_epoch.tv_nsec;
+        time_seconds = time.tv_sec;
+        time_nanoseconds = time.tv_nsec;
+
+        publish.time_generated_data = time_epoch;
+        message.data = publish;
+        strcpy(message.topic, client_topic);
+
+        printf("[%ld.%ld] Publicado mensaje topic: %s - mensaje: %s - Generó %ld.%ld\n",
+        time_seconds, time_nanoseconds, client_topic, publish.data,
+        time_seconds_epoch, time_nanoseconds_epoch);
+
+        if (send(client_socket, (void *)&message, sizeof(message), 0) < 0){
+            warnx("send() failed. %s\n", strerror(errno));
+            exit(1);
+        }
+    }
+}
+
+void transfer_subscriber(){
+    struct publish publish;
+    struct timespec time, time_epoch;
+    long received_seconds, received_nanoseconds,
+    generated_seconds, generated_nanoseconds,
+    latency_seconds, latency_nanoseconds;
+
+    while(1){
+        // Wait for data from the broker
+        if (recv(client_socket, (void *)&publish, sizeof(publish), 0) < 0){
+            warnx("recv() failed. %s\n", strerror(errno));
+            exit(1);
+        }
+
+        clock_gettime(CLOCK_REALTIME, &time);
+        clock_gettime(CLOCK_REALTIME, &time_epoch);
+        received_seconds = time_epoch.tv_sec;
+        received_nanoseconds = time_epoch.tv_nsec;
+        generated_seconds = publish.time_generated_data.tv_sec;
+        generated_nanoseconds = publish.time_generated_data.tv_nsec;
+
+        if (received_nanoseconds - generated_nanoseconds < 0){
+            latency_seconds = received_seconds - generated_seconds - 1;
+            latency_nanoseconds = received_nanoseconds - generated_nanoseconds + 1000000000;
+        } else {
+            latency_seconds = received_seconds - generated_seconds;
+            latency_nanoseconds = received_nanoseconds - generated_nanoseconds;
+        }
+
+        printf("[%ld.%ld] Recibido mensaje topic: %s - mensaje: %s - Generó: %ld.%ld"
+        "- Recibido: %ld.%ld - Latencia: %ld.%ld\n",
+        time.tv_sec, time.tv_nsec, client_topic, publish.data, 
+        generated_seconds, generated_nanoseconds, received_seconds,
+        received_nanoseconds, latency_seconds, latency_nanoseconds); 
+    }
+}
+
+void start_data_transfer(){
+
+    if (client_type == PUBLISHER){
+        transfer_publisher();
+    } else {
+        transfer_subscriber();
+    }
+}
+
+void set_name (char name[6]){
+    strcpy(proc_name,name);
+};
+
+void set_ip_port (char* ip, unsigned int port){
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    DEBUG_PRINTF("%s: IP set: %s | Port set: %d \n",proc_name ,ip, port);
+};
+
+int socket_create(){
+    setbuf(stdout, NULL);
+    int socket_ = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_ < 0){
+        warnx("%s: Error creating socket. %s\n",proc_name ,strerror(errno));
+        exit(1); 
+    }
+    DEBUG_PRINTF("%s: Socket successfully created...\n",proc_name );
+
+    return socket_;
+};
+
+void socket_connect(int socket_){
+    if (connect(socket_, (struct sockaddr *)&addr, sizeof(addr)) != 0){
+        DEBUG_PRINTF("Error connecting\n");
+        exit(EXIT_FAILURE);
+    }
+    DEBUG_PRINTF ("%s: Connected to server...\n",proc_name );
+};
+
+void talk_to_server(char *topic, char *ip, int port){
+
+    DEBUG_PRINTF("TALK\n");
+
+    if (strcmp(proc_name,"Publisher") == 0){
+        DEBUG_PRINTF("PUBLISHER\n");
+        client_type = PUBLISHER;
+    } else {
+        DEBUG_PRINTF("SUBSCRIBER\n");
+        client_type = SUBSCRIBER;
+    }
+
+    strcpy(client_topic, topic);
+
+    struct timespec time;
+    long time_seconds;
+    long time_nanoseconds;
+
+    struct response response;
+
+    struct message message;
+    strcpy(message.topic, topic);
+
+    if (client_type == PUBLISHER){
+        DEBUG_PRINTF("ACTION %d\n", REGISTER_PUBLISHER);
+        message.action = REGISTER_PUBLISHER;
+    } else {
+        DEBUG_PRINTF("ACTION %d\n", REGISTER_SUBSCRIBER);
+        message.action = REGISTER_SUBSCRIBER;    
+    }
+
+    client_socket = socket_create();
+
+    socket_connect(client_socket);
+
+    clock_gettime(CLOCK_REALTIME, &time);
+    time_seconds = time.tv_sec;
+    time_nanoseconds = time.tv_nsec;
+
+    if (client_type == SUBSCRIBER){
+        printf("[%ld.%ld] %s conectado con broker (%s:%d)\n",
+        time_seconds, time_nanoseconds, proc_name, ip, port);
+    } else {
+        printf("[%ld.%ld] %s conectado con broker correctamente\n",
+        time_seconds, time_nanoseconds, proc_name);
+    }
+
+    DEBUG_PRINTF("action: %d, topic: %s\n",message.action, message.topic);  
+    if (send(client_socket, (void *)&message, sizeof(message), 0) < 0){
+        warnx("send() failed. %s\n", strerror(errno));
+        exit(1);
+    }
+
+    if (recv(client_socket, (void *)&response, sizeof(response), 0) < 0){
+        warnx("recv() failed. %s\n", strerror(errno));
+        exit(1);
+    }
+
+    client_id = response.id;
+
+    clock_gettime(CLOCK_REALTIME, &time);
+    time_seconds = time.tv_sec;
+    time_nanoseconds = time.tv_nsec;
+    if (response.response_status == OK){
+        printf("[%ld.%ld] Registrado correctamente con ID: %d para topic %s\n",
+        time_seconds, time_nanoseconds, response.id, topic);    
+    } else {
+        printf("[%ld.%ld] Error al hacer el registro: error=%d\n",
+        time_seconds, time_nanoseconds, response.response_status);
+        exit(1);       
+    }
+
+    DEBUG_PRINTF("Message sent\n"); 
+
+    start_data_transfer();
+
+}
---
+++ practica4/proxy_client.h
@@ -0,0 +1,87 @@
+// Gonzalo Vega Pérez - 2022
+
+#ifndef PROXY_H
+#define PROXY_H
+
+#ifdef DEBUG
+    #define DEBUG_PRINTF(...) printf("DEBUG: "__VA_ARGS__)
+#else
+    #define DEBUG_PRINTF(...)
+#endif
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include <errno.h>
+#include <string.h>
+#include <err.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <getopt.h>
+
+#define PUBLISHER 0
+#define SUBSCRIBER 1
+
+enum operations {
+REGISTER_PUBLISHER = 0,
+UNREGISTER_PUBLISHER,
+REGISTER_SUBSCRIBER,
+UNREGISTER_SUBSCRIBER,
+PUBLISH_DATA
+};
+
+struct publish {
+    struct timespec time_generated_data;
+    char data[100];
+};
+
+struct message {
+    enum operations action;
+    char topic[100];
+    // Solo utilizado en mensajes de UNREGISTER
+    int id;
+    // Solo utilizado en mensajes PUBLISH_DATA
+    struct publish data;
+};
+
+enum status {
+    ERROR = 0,
+    LIMIT,
+    OK
+};
+
+struct response {
+    enum status response_status;
+    int id;
+};
+
+void transfer_publisher();
+
+void transfer_subscriber();
+
+void start_data_transfer();
+
+void sighandler(int signum);
+
+void talk_to_server(char* topic, char *ip, int port);
+
+// Establece el nombre del proceso (para los logs y trazas)
+void set_name (char name[6]);
+
+// Establecer ip y puerto
+void set_ip_port (char* ip, unsigned int port);
+
+// Establece conexión con el servidor.
+void socket_connect(int socket_);
+
+// Crea un socket para TCP
+int socket_create();
+
+
+#endif // PROXY_H
